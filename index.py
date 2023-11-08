d3800818da0b2283db0283d5c7125f61690d8315
jzkheng2807 <jiazheng2807@gmail.com>
2023-11-08 16:26:58 +0800

    make it better

--- index.py
+++ index.py
@@ -62,7 +62,7 @@ def input():
 @app.route("/search", methods=["GET", "POST"])
 def search():
     if request.method == "POST":
-    	keyword = request.form["user"]
+    	keyword = request.form["keyword"]
     	result = "您輸入的帳號是：" + keyword
     	Result += "<br>"
     	db = firestore.client()
