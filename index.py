c8975ffb01b84792e89becdb5fc837252acfcbf5
jzkheng0728 <jiazheng2807@gmail.com>
2023-11-22 15:51:14 +0800

    make it better

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import requests
+from bs4 import BeautifulSoup
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
