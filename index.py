e429c76cad3efece15ea0ae8cf03a2b22f2765a7
jzkheng0728 <jiazheng2807@gmail.com>
2023-12-06 15:06:26 +0800

    make it better

--- index.py
+++ index.py
@@ -185,7 +185,6 @@ def search3():
 def data():
 	url = "https://datacenter.taichung.gov.tw/swagger/OpenData/db36e286-1d2b-4784-99b9-3b0790dd9652"
 	Data = requests.get(url)
-	Data.encoding = "utf-8"
 	JsonData = json.loads(Data.text)
 	if request.method=="POST":
 		Road = request.form["Road"]
