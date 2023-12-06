aa6f9d771cb3cebd74648565ac5273210b846012
jzkheng0728 <jiazheng2807@gmail.com>
2023-12-06 14:54:13 +0800

    make it better

--- index.py
+++ index.py
@@ -185,17 +185,19 @@ def search3():
 def data():
 	url = "https://datacenter.taichung.gov.tw/swagger/OpenData/db36e286-1d2b-4784-99b9-3b0790dd9652"
 	Data = requests.get(url)
-	#print(Data.text)
+	Data.encoding = "utf-8"
 	JsonData = json.loads(Data.text)
-	Result = ""
-	Road = input("請輸入欲查詢的路名：")
-	for item in JsonData:
-		if Road in item["路口名稱"]:
-			Result += item["路口名稱"] + "：發生" + item["總件數"] + "件，主因是" + item["主要肇因"] + "\n\n"
+	if request.method=="POST":
+		Road = request.form["Road"]
+		Result = ""
+		for item in JsonData:
+			if Road in item["路口名稱"]:
+				Result += item["路口名稱"] + "：發生" + item["總件數"] + "件，主因是" + item["主要肇因"] + "<br>"
 			if Result == "":
 				Result = "抱歉，查無相關資料！"
 				return Result
 			else:
 				return render_template("data.html")
+				
 if __name__ == "__main__":
 	app.run(debug=True)
